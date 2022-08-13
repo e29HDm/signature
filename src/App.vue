3afb79e283e727cb743662a5ced75e237ec4c08b
e29HDm <delignekylian@gmail.com>
2022-08-13 15:23:36 +0200

    boostrap project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <main class="px-3">
+    <header>
+      <nav class="flex items-center justify-between">
+        <h1 class="text-2xl font-mono">Signature</h1>
+        <div class="flex space-x-6">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </div>
+      </nav>
+    </header>
+
+    <RouterView class="mt-10" />
+  </main>
+
+</template>
